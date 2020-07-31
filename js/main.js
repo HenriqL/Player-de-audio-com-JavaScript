1f7ddab19514816e13ddab870bf6462a71f59cd6
Henrique Lopes <henriquelopesls032@gmail.com>
2020-07-31 12:27:40 -0300

    refactoring main js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,14 @@ const player = {
     title: document.querySelector(".card-content h6"),
     artist: document.querySelector(".artist"),
     audio: document.querySelector("audio"),
-
-}
-
-
-
-
-const data ={
-    title:"Como começei a programar | Por que criamos a Rocketseat | Nossa Stack",
-    artist: "Diego Fernandes",
-    file: "js/files/como-comecei.mp3"
+    data: {
+        title:"Como começei a programar | Por que criamos a Rocketseat | Nossa Stack",
+        artist: "Diego Fernandes",
+        file: "js/files/como-comecei.mp3"
+    }
 };
 
-title.innerHTML = data.title;
-artist.innerHTML = `Apresentado por: ${data.artist}`;
-audio.src = data.file;
+
+player.title.innerHTML = player.data.title;
+player.artist.innerHTML = `Apresentado por: ${player.data.artist}`;
+player.audio.src = player.data.file;
