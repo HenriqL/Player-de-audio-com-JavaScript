95175ca98ffd086281b8830d5a005ab100885a86
Henrique Lopes <henriquelopesls032@gmail.com>
2020-07-31 11:20:54 -0300

    Upload

---
+++ js/data.js
@@ -0,0 +1,19 @@
+
+window.audios = [
+    {
+      title:
+        "Como começei a programar / Por que criamos a Rocketseat / Nossa Stack",
+      artist: "Diego Fernandes",
+      file: "como-comecei.mp3"
+    },
+    {
+      title: "5 dicas para uma carreira sólida como programador",
+      artist: "Diego Fernandes",
+      file: "5-dicas-para-uma-carreira-solida-como-programador.mp3"
+    },
+    {
+      title: "Júnior Pleno ou Sênior, qual a diferença?",
+      artist: "Diego Fernandes",
+      file: "junior-pleno-ou-senior-qual-a-diferenca.mp3"
+    }
+  ];
