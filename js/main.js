644678fb5e671b71a41303f122dc0b329c51868a
Henrique Lopes <henriquelopesls032@gmail.com>
2020-07-31 12:31:30 -0300

    refactoring main js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,13 @@ const player = {
         title:"Como começei a programar | Por que criamos a Rocketseat | Nossa Stack",
         artist: "Diego Fernandes",
         file: "js/files/como-comecei.mp3"
-    }
+    },
+
+    start() {
+        player.title.innerHTML = player.data.title;
+        player.artist.innerHTML = `Apresentado por: ${player.data.artist}`;
+        player.audio.src = player.data.file;
+        }
 };
 
-
-player.title.innerHTML = player.data.title;
-player.artist.innerHTML = `Apresentado por: ${player.data.artist}`;
-player.audio.src = player.data.file;
+player.start()
